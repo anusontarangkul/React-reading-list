66c98550a51ccece2cf20495c5ce873b7acc80e3
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-09-10 01:09:19 -0700

    add booklist and navbar component

--- context-app/src/App.js
+++ context-app/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import NavBar from './components/Navbar';
+import BookList from './components/BookList';
 
 function App() {
   return (
     <div className="App">
-
+      <NavBar />
+      <BookList />
     </div>
   );
 }
