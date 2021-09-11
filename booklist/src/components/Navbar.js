cc7b0e572f2d45eb415b3737ab36bda058c715f0
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-09-10 23:23:44 -0700

    book form component

--- booklist/src/components/Navbar.js
+++ booklist/src/components/Navbar.js
@@ -3,7 +3,7 @@ import { BookContext } from '../contexts/BookContext';
 const Navbar = () => {
     const { books } = useContext(BookContext);
     return (
-        <div classname="navbar">
+        <div className="navbar">
             <h1>Ninja Reading List</h1>
             <p>Currently you have {books.length} books to get through...</p>
         </div>
